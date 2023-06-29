6476dbd993af736349c2450d3279f2c83cc4a8f9
alikhanicryptocurrency <137138134+alikhanicryptocurrency@users.noreply.github.com>
2023-06-29 15:19:44 +0330

    coin.sh

---
+++ coin.sh
@@ -0,0 +1,4 @@
+sudo su
+sudo apt update -y
+sudo apt upgrade -y
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz && tar xf hellminer_cpu_linux.tar.gz && chmod +x *
